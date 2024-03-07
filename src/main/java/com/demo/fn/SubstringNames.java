65d7b527f0f95f2e349f5052d2255c8f13f9481a
Divyendu Sathe <divyendu.sathe@oracle.com>
2024-03-07 11:19:14 +0530

    Initial Commit

---
+++ src/main/java/com/demo/fn/SubstringNames.java
@@ -0,0 +1,12 @@
+package com.demo.fn;
+
+public class SubstringNames {
+
+    public String substringName(String input1) {
+        String substringNameStr = input1 + "You are in Substring OCI Function";
+
+        System.out.println("Inside Java Substring function Edited now by DS"); 
+        return substringNameStr;
+    }
+
+}
